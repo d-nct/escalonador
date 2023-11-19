f0d0c69963d6fb50994c59245456556f5f9e77a7
Daniel Nocito <nctdaniel1@gmail.com>
2023-11-19 00:31:20 -0300

    Cria as primeiras configurações do simulador

---
+++ src/header.h
@@ -0,0 +1,20 @@
+/*
+Arquivo: header.h
+
+Unidades:
+- tempo: milissegundos
+*/
+
+/* Configurações gerais do Escalonador */
+#define MAX_PROCESSOS               50  
+#define FATIA_DE_TEMPO              
+
+/* Define os tipos de IO aceitos pelo simulador */
+#define IO_DISCO_COD                1
+#define IO_FITA_COD                 2
+#define IO_IMPRESSORA_COD           3
+
+/* Define o tempo necessário para cada tipo de I/O */
+#define IO_DISCO_TEMPO              
+#define IO_FITA_TEMPO               
+#define IO_IMPRESSORA_TEMPO          
