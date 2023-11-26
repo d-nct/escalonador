d3f13ab05edc573f37567bc6e98570ff35e398f7
user name <eduardasm@dcc.ufrj.br>
2023-11-25 21:27:13 -0300

    change filaInsere & filaRemove

--- src/fila.c
+++ src/fila.c
@@ -11,57 +11,56 @@ void filaInit(Fila* fila) {
     fila->fim = NULL;
 }
 
-void filaInsere(Fila* fila, int processo) {
-    /* cria o novo no */
-    No* novo = (No*) malloc(sizeof(No));
+void filaInsere(Fila* fila, PCB* processo) {
+    // cria o novo nó 
+    No* novo = (No*)malloc(sizeof(No));
     if (novo == NULL) {
         printf("Sem memória disponível\n");
         exit(1);
     }
-    
-    /* configura o no */
+
+    // configura o nó 
     novo->processo = processo;
     novo->prox = NULL;
 
-    /* poe na fila */
+    // coloca na fila 
     if (fila->inicio == NULL) {
-        /* caso fila vazia */
+        // caso fila vazia 
         fila->inicio = novo;
         fila->fim = novo;
     } else {
-        /* caso fila não vazia */
-        fila->fim->prox = novo; /* linka na fila */
-        fila->fim = novo; /* atualiza a ref do fim para o novo */
+    //caso fila não vazia 
+        fila->fim->prox = novo; // linka na fila 
+        fila->fim = novo;       // atualiza a referência do fim para o novo 
     }
 }
 
-int filaRemove(Fila* fila) {
-    int primeiro;
+PCB* filaRemove(Fila* fila) {
+    PCB* primeiro;
     No* tmp;
 
-    /* verifica se há pelo menos um cara */
+    // verifica se há pelo menos um nó 
     if (fila->inicio == NULL) {
         printf("Fila vazia!!!\n");
-        return 0;
+        return NULL;
     }
 
-    /* pega o primeiro cara */
-    tmp = fila->inicio; /* para futura remoção*/
+    // pega o primeiro nó
+    tmp = fila->inicio; // aux p/ futura remoção
     primeiro = fila->inicio->processo;
 
-    /* atualiza a ref do inicio */
     fila->inicio = fila->inicio->prox;
 
-    /* vê se a fila ficou vazia */
+    // checa se a fila ficou vazia 
     if (fila->inicio == NULL) {
         fila->fim = NULL;
     }
 
-    /* libera a memória do nó */
     free(tmp);
 
     return primeiro;
 }
+
 int estaVazia(Fila *fila) {
     return (fila->inicio == NULL) ? 1 : 0; // Retorna 1 se estiver vazia e 0 se estiver com pelo menos um processo
 }
